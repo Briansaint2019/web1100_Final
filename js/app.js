f6c44d98e8a8f80d1a833e22ae9320b9155a4c6e
Briansaint2019 <briansaint88@outlook.com>
2018-07-23 19:29:20 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
 const dog_api = 'https://dog.ceo/api/'
+const viewDog = document.querySelector(".viewDog")
+
+class Dogbreed {
+    constructor() {
+        // this.message = message
+        this.dogChoice()
+    }
+    dogChoice() {
+        $.ajax({
+            url: "https://dog.ceo/api/breeds/list/all",
+            dataType: "json",
+            success: data => {
+                // this.message = data
+                // this.setChoice(this.breed)
+               this.displayInfo(data)
+                console.log(data)
+            },
+            error: error => {
+                console.log("There Was An Error")
+            }
 
 
-viewDog.addEventListener('click', random())
+        })
 
-const random = function randomDog(){
-    //This is the code
+    }
+    displayInfo(data) {
+        
+        `
+     <select name="" id="selectBreed"><option value=${data.name}></option></select>
+     `
+    }
 }
+
+
+const bre = new Dogbreed()
+// viewDog.addEventListener('click', random())
+
+// const random = function randomDog() {
+    //This is the code
+
+// }
